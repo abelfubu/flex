ad61a1cdad8147c44fb01cbc7cebc6af5a6c8128
abelfubu <abelfubu@gmail.com>
2021-04-16 13:59:11 +0200

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import './App.scss'
 
 import React, { useState } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 
 import Header from './components/header/Header'
 import Flexbox from './pages/flexbox/Flexbox'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='App'>
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <Route exact path='/' component={Flexbox} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
